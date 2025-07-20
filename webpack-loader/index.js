12e01f2133066f1774472f7f5d564b9d17148a92
ashish <ashishnnnnn@gmail.com>
2025-07-20 11:14:23 +0530

    feat: added loader in webpack

---
+++ webpack-loader/index.js
@@ -0,0 +1,17 @@
+import mountainImg from "./assets/mountain.webp";
+import bridgeImg from "./assets/bridge.png";
+
+const mountain = document.getElementById("mountiain")
+const bridge = document.getElementById("bridge")
+
+mountain.style.visibility = 'hidden'
+bridge.style.visibility = 'hidden'
+
+console.log("i am running")
+
+document.getElementById("button").addEventListener("click", () => {
+    mountain.style.visibility = 'visible'
+    bridge.style.visibility = 'visible'
+    mountain.src = mountainImg
+    bridge.src = bridgeImg
+})
---
+++ webpack-loader/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: "asset/[hash][ext]",
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(webp|png)$/,
+                type: "asset/resource"
+            }
+        ]
+    },
+    mode: "development"
+};
